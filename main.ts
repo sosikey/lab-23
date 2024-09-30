f23dfa21b32c8dc256fbe991227b1f6d2da4644b
sosikey <t.winston@mcaabhm.org>
2024-09-30 12:59:18 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+	
+})
 let number_1 = game.askForNumber("first number")
 let number_2 = game.askForNumber("second number")
 game.splash("" + number_1 + "/" + number_2 + "=" + 58 % 10000)
